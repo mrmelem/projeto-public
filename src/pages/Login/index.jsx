7bcb80f23d706857a018116db9875bc4b1b25953
Pedro Melém <Melemredes@gmail.com>
2021-06-14 18:01:56 -0300

    Atualização do Readme.md

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,13 @@
 import React, { useContext } from 'react';
 import UserContext from '../../utils/Contexts/UserContext';
 import { useHistory } from 'react-router';
+import api from '../../utils/API/axios';
 
 import Navbar from '../../components/Navbar';
 import Center from '../../components/Center';
 import LoginForm from '../../components/Forms/LoginForm';
 
 const Login = () => {
-    function UserLogin({ user, key }) {
-        if (user === 'pedromelem' && key === 'ph193456') return { token: '123' };
-        return { error: "Usuário ou senha inválida" };
-    }
 
     const history = useHistory()
 
@@ -23,7 +20,7 @@ const Login = () => {
             <Navbar />
 
             <Center>
-                <LoginForm UserLogin={UserLogin} />
+                <LoginForm handleLogin={UserLogin} />
             </Center>
         </>
     )
